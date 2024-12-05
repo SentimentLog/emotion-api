da145cba30fbff5cafbc940f96a614c728318377
UICHEOL-HWANG <icuchoel@gmail.com>
2024-12-05 16:07:21 +0900

    Feat: 기초 틀 세팅 완료

---
+++ src/main/java/icucheol/emotion_analyzer/EmotionAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package icucheol.emotion_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotionAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmotionAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/icucheol/emotion_analyzer/EmotionAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package icucheol.emotion_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotionAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
